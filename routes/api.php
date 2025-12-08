fe0cc631b0f2bf055d242e31acaa719617972fa0
meotraa <putraa.aditiaa1@gmail.com>
2025-12-08 08:09:51 +0700

    FIX Sanctum

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\Api\TlJabarController;
 use App\Http\Controllers\Api\TlKabController;
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::get('/tl-bpk', [TlBpkController::class, 'index']);
-    Route::get('/tl-jabar', [TlJabarController::class, 'index']);
-    Route::get('/tl-kab', [TlKabController::class, 'index']);
+    Route::get('/tl/bpk', [TlBpkController::class, 'index']);
+    Route::get('/tl/jabar', [TlJabarController::class, 'index']);
+    Route::get('/tl/kab', [TlKabController::class, 'index']);
 });
